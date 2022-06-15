b0c7e6bd5ee145d5341f541f43ed6b8e258c0993
Artem Slipets <someboygit@gmail.com>
2022-06-15 20:33:10 +0300

    Solution Collection 2

--- src/com/javarush/task/task31/task3102/Solution.java
+++ src/com/javarush/task/task31/task3102/Solution.java
@@ -1,19 +1,34 @@
 package com.javarush.task.task31.task3102;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 
 /* 
 Находим все файлы
 */
 
 public class Solution {
-    public static List<String> getFileTree(String root) throws IOException {
-        return null;
+    public static List<String> getFileTree(String root) {
+        File rootDirectory = new File(root);
+        List<String> fileList = new ArrayList<>();
+        Queue<File> fileTree = new PriorityQueue<>();
 
+        Collections.addAll(fileTree, Objects.requireNonNull(rootDirectory.listFiles()));
+        while (!fileTree.isEmpty()) {
+            File currentFile = fileTree.remove();
+            if (currentFile.isDirectory()) {
+                Collections.addAll(fileTree, Objects.requireNonNull(currentFile.listFiles()));
+            } else {
+                fileList.add(currentFile.getAbsolutePath());
+            }
+        }
+        return fileList;
     }
 
     public static void main(String[] args) {
-
+        ArrayList<String> list = new ArrayList<>(getFileTree("/home/someboy/Документы"));
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
